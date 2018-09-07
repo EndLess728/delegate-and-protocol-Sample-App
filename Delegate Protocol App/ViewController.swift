d66377b425c73a9c714b78fc037b964e3ab447e3
EndLess728 <aryanmk.mk5@gmail.com>
2018-09-07 16:43:41 +0530

    Delegate and Protocol Sample App

--- Delegate Protocol App/ViewController.swift
+++ Delegate Protocol App/ViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , dataPass {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func goToFormButton(sender: AnyObject) {
+        
+        let secondVC = self.storyboard?.instantiateViewControllerWithIdentifier("second") as! secondViewController
+        secondVC.delegate = self
+        self.navigationController?.pushViewController(secondVC, animated: true)
+        
     }
+    
+    func dataPassing(name: String, age: String, city: String) {
+        nameLabel.text = name
+        ageLabel.text = age
+        cityLabel.text = city
+    }
+    
+
 
 
 }
---
+++ Delegate Protocol App/secondViewController.swift
@@ -0,0 +1,49 @@
+//
+//  secondViewController.swift
+//  Delegate Protocol App
+//
+//  Created by MacMini on 9/7/18.
+//  Copyright © 2018 com.omgmk. All rights reserved.
+//
+
+import UIKit
+
+protocol dataPass {
+    func dataPassing( name:String, age:String, city:String)
+}
+
+class secondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameTxt: UITextField!
+
+    
+    @IBOutlet weak var ageTxt: UITextField!
+    
+
+    @IBOutlet weak var cityTxt: UITextField!
+    
+    
+    var delegate : dataPass! // we use ! sing bcz if the value of datapass is nill ot empty it will crash the app but if we use ? instead of this then if the value will become nil or it has any type of dta the app will not crash
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    @IBAction func saveButton(sender: UIButton) {
+        
+        delegate.dataPassing(nameTxt.text!, age: ageTxt.text!, city: cityTxt.text!)
+        
+    }
+    
+    
+    
+    
+
+
+}
